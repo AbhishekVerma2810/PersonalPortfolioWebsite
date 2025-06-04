d5f61a04b7d38b6f8967359c78216f9ebbaa5ca1
Abhishek Verma <28a.v10@gmail.com>
2025-06-04 15:56:47 +0530

    Add project files.

---
+++ PersonalPortfolioWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalPortfolioWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
